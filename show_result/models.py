072bf1db06700fc24ee88277e84d921651e1884d
Bojun Wu <bojun0817@gmail.com>
2021-12-28 03:29:24 +0800

    add more detail to house data

--- show_result/models.py
+++ show_result/models.py
@@ -9,6 +9,9 @@ class house(models.Model):
     lon = models.FloatField('經度', blank=True, null=True)
     unitPrice = models.FloatField('每平方公尺單價', blank=True)
     update_time = models.DateTimeField('更新時間', auto_now=True)
+    age = models.IntegerField('屋齡', blank=True, null=True)
+    building_state = models.CharField('建物型態', max_length=256, blank=True)
+    transaction_sign = models.CharField('交易標的', max_length=256, blank=True)
 
     def __str__(self):
         return '地址: %s 每平方公尺售價: %s' % (self.position, self.unitPrice)
